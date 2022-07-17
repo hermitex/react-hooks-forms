898ff516715d6b86aaccd8420cc09c73523b1e7f
hermitex <mercytibs@yahoo.com>
2022-07-17 16:08:35 +0300

    implement learning objectives

---
+++ src/components/ParentComponent.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import Form from "./Form";
+
+function ParentComponent() {
+  const [firstName, setFirstName] = useState("John");
+  const [lastName, setLastName] = useState("Doe");
+
+  function handleFirstNameChange(event) {
+    setFirstName(event.target.value);
+  }
+  function handleLastNameChange(event) {
+    setLastName(event.target.value);
+  }
+
+  return (
+    <Form
+      firstName={firstName}
+      lastName={lastName}
+      handleFirstNameChange={handleFirstNameChange}
+      handleLastNameChange={handleLastNameChange}
+    />
+  );
+}
+
+export default ParentComponent;
